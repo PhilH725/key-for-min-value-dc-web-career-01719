31d6eb3c072ca14c4381930980c27e99fc515117
Phil Hartley <philh725@gmail.com>
2018-12-19 03:12:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  sorter = []
+  while sorter.length != 1
+    counter = 1
+    name_hash.each do |name, amount|
+      if counter = 1
+        sorter << {name => amount}
+      else
+        if amount < sorter[0]
+          sorter << {name => amount}
+        end
+      end
+    end
+    if sorter.length != 1
+      sorter = []
+    end
+  end
 end
